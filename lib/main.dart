c5a2bd33b9c4990037e3634c671c293df5abe128
fuseSavage <chaiwat.si.61@ubu.ac.th>
2020-12-13 17:15:40 +0700

    add

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 
+
 void main() {
   runApp(MyApp());
 }
@@ -7,72 +8,105 @@ void main() {
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
+    Widget titleSection = Container(
+      padding: const EdgeInsets.all(32),
+      child: Row(
+        children: [
+          Expanded(
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                Container(
+                  padding: const EdgeInsets.only(bottom: 8),
+                  child: Text(
+                    'Computer Devalopment History',
+                    style: TextStyle(
+                      fontWeight: FontWeight.bold,
+                    ),
+                  ),
+                ),
+                Text(
+                  'By Chaiwat Singkibut',
+                  style: TextStyle(
+                    color: Colors.grey[500],
+                  ),
+                ),
+              ],
+            ),
+          ),
+          Icon(
+            Icons.star,
+            color: Colors.red[500],
+          ),
+          Text('41'),
+        ],
+      ),
+    );
+
+    Color color = Theme.of(context).primaryColor;
+
+    Widget buttonSection = Container(
+      child: Row(
+        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+        children: [
+          _buildButtonColumn(color, Icons.call, 'CALL'),
+          _buildButtonColumn(color, Icons.near_me, 'ROUTE'),
+          _buildButtonColumn(color, Icons.share, 'SHARE'),
+        ],
+      ),
+    );
+
+    Widget textSection = Container(
+      padding: const EdgeInsets.all(32),
+      child: Text(
+        '       A computer is a machine that can be instructed to carry out sequences of arithmetic or logical operations automatically via computer programming. Modern computers have the ability to follow generalized sets of operations, called programs. These programs enable computers to perform an extremely wide range of tasks. A "complete" computer including the hardware, the operating system (main software), and peripheral equipment required and used for "full" operation can be referred to as a computer system. This term may as well be used for a group of computers that are connected and work together',
+        softWrap: true,
+      ),
+    );
+
     return MaterialApp(
-      title: 'mobile-test',
+      title: 'Flutter layout demo',
       home: Scaffold(
         appBar: AppBar(
-          title: Text('Computer History'),
+          title: Text('Flutter layout demo'),
         ),
-        body: Column(
-          mainAxisAlignment: MainAxisAlignment.spaceAround,
+        body: ListView(
           children: [
-            Center(
-              child: Image.asset(
-                'images/bg.jpg',
-                fit: BoxFit.cover,
-              ),
+            Image.asset(
+              'images/bg.jpg',
+              width: 600,
+              height: 240,
+              fit: BoxFit.cover,
             ),
-            Row(
-              mainAxisAlignment: MainAxisAlignment.spaceAround,
-              children: [
-                Column(
-                  children: [
-                    Text(
-                      'Computer Devalopment History',
-                      style: TextStyle(fontSize: 18, color: Colors.black),
-                    ),
-                    Text(
-                      'By Chaiwat Singkibut',
-                      style: TextStyle(fontSize: 14, color: Colors.black),
-                    ),
-                  ],
-                ),
-                Row(
-                  children: [Icon(Icons.star, color: Colors.red), Text('42M')],
-                ),
-              ],
-            ),
-            Row(
-              mainAxisAlignment: MainAxisAlignment.spaceAround,
-              children: [
-                Column(
-                  children: [
-                    Icon(Icons.call, color: Colors.blue),
-                    Text('CALL')
-                  ],
-                ),
-                Column(
-                  children: [
-                    Icon(Icons.router, color: Colors.blue),
-                    Text('ROUTER')
-                  ],
-                ),
-                Column(
-                  children: [
-                    Icon(Icons.share, color: Colors.blue),
-                    Text('SHARE')
-                  ],
-                ),
-              ],
-            ),
-            Padding(
-              padding: EdgeInsets.all(15),
-              child: Text(
-                  '     A computer is a machine that can be instructed to carry out sequences of arithmetic or logical operations automatically via computer programming. Modern computers have the ability to follow generalized sets of operations, called programs. These programs enable computers to perform an extremely wide range of tasks. A "complete" computer including the hardware, the operating system (main software), and peripheral equipment required and used for "full" operation can be referred to as a computer system. This term may as well be used for a group of computers that are connected and work together, in particular a computer network or computer cluster.'),
-            ),
+            titleSection,
+            buttonSection,
+            textSection,
           ],
         ),
       ),
     );
   }
+
+  Column _buildButtonColumn(Color color, IconData icon, String label) {
+    return Column(
+      mainAxisSize: MainAxisSize.min,
+      mainAxisAlignment: MainAxisAlignment.center,
+      children: [
+        Icon(icon, color: color),
+        Container(
+          margin: const EdgeInsets.only(top: 8),
+          child: Text(
+            label,
+            style: TextStyle(
+              fontSize: 12,
+              fontWeight: FontWeight.w400,
+              color: color,
+            ),
+          ),
+        ),
+      ],
+    );
+  }
 }
+
+
